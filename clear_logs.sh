09c67804e21603798751d18b1fb940cbd886f307
vajierik <otr580@gmail.com>
2023-11-17 01:41:18 +0300

    modify

--- clear_logs.sh
+++ clear_logs.sh
@@ -2,9 +2,9 @@
 
 # clear Admin server logs
 cd /oracle/admin/OSBDomain/aserver/OSBDomain/servers/AdminServer/logs
-find . -name 'OSBDomain.log2*' -mtime +5 -exec rm -f {} \; 2>/dev/null
-find . -name 'OSBDomain.log0*' -mtime +5 -exec rm -f {} \; 2>/dev/null
-find . -name 'AdminServer.log2*' -mtime +5 -exec rm -f {} \; 2>/dev/null
+find . -name 'OSBDomain.log2*' -mtime +5  -exec rm -f {} \; 2>/dev/null
+find . -name 'OSBDomain.log0*' -mtime +4 -exec rm -f {} \; 2>/dev/null
+find . -name 'AdminServer.log2*' -mtime +4 -exec rm -f {} \; 2>/dev/null
 
 # clear managed server logs
 cd /oracle/admin/OSBDomain/mserver/OSBDomain/servers/osb_server1/logs
@@ -14,4 +14,3 @@ find . -name 'osb_server1.out0*' -mtime +3 -exec rm -f {} \; 2>/dev/null
 find . -name 'access.log0*' -mtime +3 -exec rm -f {} \; 2>/dev/null
 cat osb_server1.out >> osb_server1.out0_`date +%Y.%m.%d-%H.%M` && cat /dev/null > osb_server1.out
 #cat /dev/null > osb_server1.out
-
